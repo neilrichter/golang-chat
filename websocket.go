e92279ab9ff2732a13821c1e819d181a78a7039d
SickMyDucks <me@neilrichter.com>
2018-03-30 13:14:40 +0200

    Now removes connection from the hub on disconnection

--- websocket.go
+++ websocket.go
@@ -17,26 +17,36 @@ type Data struct {
 	New      string `json:"new"`
 }
 
+var count = 0
+var hub = make(map[int]*websocket.Conn)
+
 func main() {
 	fmt.Println("Server started")
-	hub := []*websocket.Conn{}
 	http.Handle("/connws/", websocket.Handler(func(ws *websocket.Conn) {
-		hub = append(hub, ws)
+		current := connexion(ws)
+		fmt.Println("The hub:", hub)
 		var data Data
 		for {
 			err := websocket.JSON.Receive(ws, &data)
 			if err != nil {
-				fmt.Println(err)
+				delete(hub, current)
 				ws.Close()
-				break
+				fmt.Println("The hub:", hub)
+				return
 			}
 			result, _ := json.Marshal(data)
 			for _, i := range hub {
-				fmt.Println(data.Content)
-				websocket.Message.Send(i, string(result))
+				_ = websocket.Message.Send(i, string(result))
 			}
 		}
 	}))
 
 	http.ListenAndServe(":9999", nil)
+}
+
+func connexion(ws *websocket.Conn) int {
+	hub[count] = ws
+	current := count
+	count++
+	return current
 }
